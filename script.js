c5baf6b5b11cd53ac98671cc1d008b450a3403b6
DevaKumar <137104336+Deva843@users.noreply.github.com>
2023-11-15 15:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const list = document.getElementById("level");
+let parent ;
+let count = 0;
+while(parent){
+	parent = list.parentNode;
+	count++;
+}
+console.log(count);
